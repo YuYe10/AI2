b4554c69564be00467f41ac7bb4b6ecaf9cf5973
yuye <3238918486@qq.com>
2024-07-11 18:05:32 +0800

    5th commit

--- 1-mirror/mirror.py
+++ 1-mirror/mirror.py
@@ -4,12 +4,11 @@ from transformers import AutoModelForQuestionAnswering
 
 from huggingface_hub.utils import logging
 
-logging.set_verbosity(logging.DEBUG)
-logger = logging.get_logger("datasets")
+logger = logging.get_logger("G:/AI/squad")
 # implement your logging function here
 # NB: you need to set DEBUG mode with logging module
+logger.debug("This is a debug log")
 
-logger.debug("GET")
 # you only need to download the dataset
 squad = load_dataset("G:/AI/squad", split="train[:5000]")
 print(squad)
