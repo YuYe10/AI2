92bb3b17d5baba2c464c1f2451371e38b871b416
yuye <3238918486@qq.com>
2024-07-11 11:12:24 +0800

    2nd commit

--- 1-mirror/mirror.py
+++ 1-mirror/mirror.py
@@ -4,19 +4,21 @@ from transformers import AutoModelForQuestionAnswering
 
 from huggingface_hub.utils import logging
 
+logging.set_verbosity(logging.DEBUG)
+logger = logging.get_logger("datasets")
 # implement your logging function here
 # NB: you need to set DEBUG mode with logging module
 
-
+logger.debug("GET")
 # you only need to download the dataset
 squad = load_dataset("squad", split="train[:5000]")
 print(squad)
 
-"""
+'''
 model = AutoModelForQuestionAnswering.from_pretrained(
     "distilbert/distilbert-base-uncased"
 )
 
 # evaluation metric cannot be downloaded from hf-mirror
 mse = load("evaluate-metric/mse")
-"""
+'''
